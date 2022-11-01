06c1f8b21c5ecc8b9c3e5310db5378d05734ab8a
Esteban Salazar Zapata <esteban.salazar@Esteban-Salazar-MacBook-Pro.local>
2022-11-01 18:27:42 -0500

    feat(project): create template project for react

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const app = (
+	<div>
+		<h1>Esteban Salazar</h1>
+		<img src='/vite.svg' />
+	</div>
+);
+const container = document.getElementById('root');
+
+ReactDOM.render(app, container);
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
